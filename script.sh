53f5d5e73b7bdc26ab524032ef3ca6d1e045ed9c
snaranjoa <50253439+snaranjoa@users.noreply.github.com>
2019-05-14 01:46:23 -0500

    Proceso

--- script.sh
+++ script.sh
@@ -22,9 +22,30 @@ awk 'BEGIN{print "FECHA;HHMMSS;DIR;VEL;EST"}(NR>1){print $0}' estaciones.csv > e
 tr ',' '.' < estaciones1.csv > estaciones2.csv
 tr ';' ',' < estaciones2.csv > estaciones3.csv
 
-#Obtener titulos
-csvcut -n estaciones3.csv
-
 #Organizar datos y campos
 sed 's;/\([0-9][0-9]\),;/20\1,;' estaciones3.csv > estaciones4.csv
-head estaciones4.csv
+
+#Incluir ano, mes y hora
+sed 's;/\([0-9][0-9]\)/\([0-9][0-9][0-9][0-9]\),;/\1/\2,\1,\2,;' estaciones4.csv > estaciones5.csv
+sed 's/,\([0-9][0-9]\):/,\1,\1:/' estaciones5.csv > estaciones6.csv
+awk 'BEGIN{print "FECHA,MES,ANO,HORA,HHMMSS,DIR,VEL,EST"}(NR>1){print $0}' estaciones6.csv > finalestaciones.csv
+head finalestaciones.csv
+
+#Obtener titulos
+csvcut -n finalestaciones.csv
+
+#Creacion de tablas
+csvsql --query 'select EST,MES,avg(VEL) as VELPROM from finalestaciones group by EST,MES' finalestaciones.csv > velocidad-por-mes.csv
+csvsql --query 'select EST,ANO,avg(VEL) as VELPROM from finalestaciones group by EST,ANO' finalestaciones.csv > velocidad-por-ano.csv
+csvsql --query 'select EST,HORA,avg(VEL) as VELPROM from finalestaciones group by EST,HORA' finalestaciones.csv > velocidad-por-hora.csv
+
+#Creacion de directorio resultado
+cd ..
+mkdir resultado
+mv ./estaciones/velocidad-por-mes.csv ./resultado/velocidad-por-mes.csv
+mv ./estaciones/velocidad-por-ano.csv ./resultado/velocidad-por-ano.csv
+mv ./estaciones/velocidad-por-hora.csv ./resultado/velocidad-por-hora.csv
+
+#Borrar archivos temporales
+rm ./estaciones/estacion_*.csv
+rm ./estaciones/estaciones*.csv
